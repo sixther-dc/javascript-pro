99ea070c9a50f9c51b75b5d1b58ed55757f59120
sixther <382998946@qq.com>
2019-05-14 01:01:28 +0800

    add something

--- test/test_drag1/index.js
+++ test/test_drag1/index.js
@@ -74,7 +74,20 @@ $(document).ready(function() {
       }
     }
 
+
+    function cuteHide(el) {
+        el.animate({
+            opacity: '0'
+        }, 150, function () {
+            el.animate({
+                height: '0px'
+            }, 150, function () {
+                el.remove();
+            });
+        });
+    }
+
     $(".remove-icon").click(function(){
-        $(this).parent().remove();
+        cuteHide($(this).parent());
     })
   });
