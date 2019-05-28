65ff54ece50daa3d69115aa18561f9e08f74b961
sixther <382998946@qq.com>
2019-05-28 23:29:59 +0800

    使用evalasync替代apply

--- webpack/spa/src/components/sidebar/sidebar.js
+++ webpack/spa/src/components/sidebar/sidebar.js
@@ -1,8 +1,10 @@
 import sidebarTpl from './sidebar.html';
 class sidebarCtrl {
-    constructor($rootScope, $scope) {
+    constructor($rootScope, $scope, $animate, $element) {
         this.$rootScope = $rootScope;
-        this.$scope = $scope
+        this.$scope = $scope;
+        this.$animate = $animate;
+        this.$element = $element;
         $rootScope.favoriteEndpoints = [{
                 name: "弹性云计算器"
             },
@@ -23,13 +25,19 @@ class sidebarCtrl {
             }
         ];
 //https://docs.angularjs.org/api/ng/service/$animate
+//https://blog.csdn.net/Bryans/article/details/81003350
         this.$scope.$on("test", (event, data) => {
             var aaa = angular.copy(this.$rootScope.favoriteEndpoints);
             this.$rootScope.favoriteEndpoints = [];
-            data.forEach((item)=>{
-                this.$rootScope.favoriteEndpoints.push(aaa[item]);
-            })
-            this.$scope.$apply();
+            // data.forEach((item)=>{
+            //     this.$rootScope.favoriteEndpoints.push(aaa[item]);
+            // })
+            this.$scope.$evalAsync(()=> {
+                data.forEach((item)=>{
+                    this.$rootScope.favoriteEndpoints.push(aaa[item]);
+                })
+            });
+            // this.$scope.$apply();
         })
     }
     addService() {
@@ -40,12 +48,13 @@ class sidebarCtrl {
 
     removeService(index) {
         console.log(index);
+        console.log($(this.$element.find("ul")));
         this.$rootScope.favoriteEndpoints.splice(index, 1)
         console.log(this.$rootScope.favoriteEndpoints);
     }
 }
 
-sidebarCtrl.$inject = ['$rootScope', '$scope']
+sidebarCtrl.$inject = ['$rootScope', '$scope', '$animate', '$element']
 const sidebar = {
     controller: sidebarCtrl,
     template: sidebarTpl
